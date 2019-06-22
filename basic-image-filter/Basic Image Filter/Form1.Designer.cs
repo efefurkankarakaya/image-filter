8ed7dd4ca999be38dcf15010cfa4f87cd83b60e7
efefurkankarakaya <Nuclearburn@outlook.com>
2019-06-22 22:11:08 +0300

    first commit with git bash

---
+++ basic-image-filter/Basic Image Filter/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Basic_Image_Filter
+{
+    partial class FormBIF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR = new System.Windows.Forms.PictureBox();
+            this.pictureBoxG = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRG = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlackWhite = new System.Windows.Forms.PictureBox();
+            this.buttonFilter = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlackWhite)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxOriginal.TabIndex = 0;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxR
+            // 
+            this.pictureBoxR.Location = new System.Drawing.Point(261, 0);
+            this.pictureBoxR.Name = "pictureBoxR";
+            this.pictureBoxR.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxR.TabIndex = 1;
+            this.pictureBoxR.TabStop = false;
+            // 
+            // pictureBoxG
+            // 
+            this.pictureBoxG.Location = new System.Drawing.Point(523, 0);
+            this.pictureBoxG.Name = "pictureBoxG";
+            this.pictureBoxG.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxG.TabIndex = 2;
+            this.pictureBoxG.TabStop = false;
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(785, 0);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxB.TabIndex = 3;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // pictureBoxRG
+            // 
+            this.pictureBoxRG.Location = new System.Drawing.Point(785, 262);
+            this.pictureBoxRG.Name = "pictureBoxRG";
+            this.pictureBoxRG.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxRG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRG.TabIndex = 7;
+            this.pictureBoxRG.TabStop = false;
+            // 
+            // pictureBoxRB
+            // 
+            this.pictureBoxRB.Location = new System.Drawing.Point(523, 262);
+            this.pictureBoxRB.Name = "pictureBoxRB";
+            this.pictureBoxRB.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxRB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRB.TabIndex = 6;
+            this.pictureBoxRB.TabStop = false;
+            // 
+            // pictureBoxGB
+            // 
+            this.pictureBoxGB.Location = new System.Drawing.Point(261, 262);
+            this.pictureBoxGB.Name = "pictureBoxGB";
+            this.pictureBoxGB.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxGB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGB.TabIndex = 5;
+            this.pictureBoxGB.TabStop = false;
+            // 
+            // pictureBoxBlackWhite
+            // 
+            this.pictureBoxBlackWhite.Location = new System.Drawing.Point(-1, 262);
+            this.pictureBoxBlackWhite.Name = "pictureBoxBlackWhite";
+            this.pictureBoxBlackWhite.Size = new System.Drawing.Size(256, 256);
+            this.pictureBoxBlackWhite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBlackWhite.TabIndex = 4;
+            this.pictureBoxBlackWhite.TabStop = false;
+            // 
+            // buttonFilter
+            // 
+            this.buttonFilter.Location = new System.Drawing.Point(387, 524);
+            this.buttonFilter.Name = "buttonFilter";
+            this.buttonFilter.Size = new System.Drawing.Size(130, 24);
+            this.buttonFilter.TabIndex = 2;
+            this.buttonFilter.Text = "Filter";
+            this.buttonFilter.UseVisualStyleBackColor = true;
+            this.buttonFilter.Click += new System.EventHandler(this.buttonFilter_Click);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(-1, 524);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(130, 24);
+            this.buttonSelect.TabIndex = 1;
+            this.buttonSelect.Text = "Select";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(523, 524);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(130, 24);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // FormBIF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 548);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.buttonFilter);
+            this.Controls.Add(this.pictureBoxRG);
+            this.Controls.Add(this.pictureBoxRB);
+            this.Controls.Add(this.pictureBoxGB);
+            this.Controls.Add(this.pictureBoxBlackWhite);
+            this.Controls.Add(this.pictureBoxB);
+            this.Controls.Add(this.pictureBoxG);
+            this.Controls.Add(this.pictureBoxR);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormBIF";
+            this.Text = "Basic Image Filter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlackWhite)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxR;
+        private System.Windows.Forms.PictureBox pictureBoxG;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxRG;
+        private System.Windows.Forms.PictureBox pictureBoxRB;
+        private System.Windows.Forms.PictureBox pictureBoxGB;
+        private System.Windows.Forms.PictureBox pictureBoxBlackWhite;
+        private System.Windows.Forms.Button buttonFilter;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ basic-image-filter/Basic Image Filter/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Basic_Image_Filter
+{
+    public partial class FormBIF : Form
+    {
+        string imageFile;
+        Bitmap image;
+        string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public FormBIF()
+        {
+            InitializeComponent();
+        }
+        private string CallGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = desktop;
+            dialog.Title = "Select";
+            dialog.Filter = "|*.jpg; *.jpeg; *.png; *.gif; *.bmp; *.heif; *.heic; *.tiff;";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imageFile = dialog.FileName;
+                image = new Bitmap(imageFile);
+                pictureBoxOriginal.Image = image;
+
+
+            }
+        }
+        private void buttonFilter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap imageR = new Bitmap(imageFile);
+                Bitmap imageG = new Bitmap(imageFile);
+                Bitmap imageB = new Bitmap(imageFile);
+                Bitmap imageRG = new Bitmap(imageFile);
+                Bitmap imageRB = new Bitmap(imageFile);
+                Bitmap imageGB = new Bitmap(imageFile);
+                Bitmap imageBlackWhite = new Bitmap(imageFile);
+
+                int r, g, b, a, bw;
+
+                Color pixel;
+
+                int height = image.Height;
+                int width = image.Width;
+
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        pixel = image.GetPixel(x, y);
+
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        a = pixel.A;
+                        bw = (r + g + b) / 3;
+
+                        imageR.SetPixel(x, y, Color.FromArgb(a, r, 0, 0));
+                        imageG.SetPixel(x, y, Color.FromArgb(a, 0, g, 0));
+                        imageB.SetPixel(x, y, Color.FromArgb(a, 0, 0, b));
+                        imageRG.SetPixel(x, y, Color.FromArgb(a, r, g, 0));
+                        imageRB.SetPixel(x, y, Color.FromArgb(a, r, 0, b));
+                        imageGB.SetPixel(x, y, Color.FromArgb(a, 0, g, b));
+                        imageBlackWhite.SetPixel(x, y, Color.FromArgb(a, bw, bw, bw));
+                    }
+                }
+
+                pictureBoxR.Image = imageR;
+                pictureBoxG.Image = imageG;
+                pictureBoxB.Image = imageB;
+                pictureBoxRG.Image = imageRG;
+                pictureBoxRB.Image = imageRB;
+                pictureBoxGB.Image = imageGB;
+                pictureBoxBlackWhite.Image = imageBlackWhite;
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Select an image.");
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            string path = desktop + "\\BIFimages\\";
+            System.IO.Directory.CreateDirectory(path);
+
+            try
+            {
+                pictureBoxOriginal.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxR.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxG.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxB.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxRG.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxRB.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxGB.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                pictureBoxBlackWhite.Image.Save(path + CallGuid() + ".jpg", ImageFormat.Jpeg);
+                MessageBox.Show("Filtered images have been created in BIFimages folder in desktop");
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Select an image and/or use filter button.");
+            }
+        }
+    }
+}
